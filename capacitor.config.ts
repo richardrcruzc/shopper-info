f4a8558ebb04d4320d55a7d84b047f1c65e565c6
Richard <richardrcruzc@gmail.com>
2023-03-07 16:48:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'shopper-info',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
